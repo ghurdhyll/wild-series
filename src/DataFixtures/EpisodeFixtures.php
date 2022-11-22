5c70358291625383c842ea1635d92c8d5c9acb15
ghurdhyll <guillaume.huguenel@gmail.com>
2022-11-22 10:32:31 +0100

    removed commented test code

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -46,18 +46,3 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
         ];
     }
 }
-        // for ($i = 1; $i <= 25; $i++) {
-        //     $programOfReference = $this->getReference('program_Title_' . $i);
-        //     $numberOfSeasons = count($programOfReference->getSeasons());
-        //     for ($j = 1; $j <= $numberOfSeasons; $j++) {
-        //         $seasonOfReference = $this->getReference('program_' . $programOfReference->getTitle() . '_S' . $j);
-        //         $numberOfEpisodes = rand(10, 20);
-        //         $k = 1;
-        //         while ($k <= $numberOfEpisodes) {
-        //             $episode = new Episode();
-        //             $episode->setSeason($seasonOfReference);
-        //             $episode->setTitle($faker->sentence($faker->numberBetween(1, 8)));
-        //             $episode->setNumber($k);
-        //             $episode->setSynopsis($faker->paragraphs(3, true));
-        //             $manager->persist($episode);
-        //             $k++;
